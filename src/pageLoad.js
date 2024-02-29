6af1852ee1ee1a32f81f2564712ed1bdc393e6e1
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-02-29 15:42:49 +0000

    complete all logic to generate each page

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -380,5 +380,61 @@ function pageLoad() {
     aboutContainer.append(aboutTitle, about);
     leftCol.append(aboutContainer);
 
-    //date picker elem.min = new Date().toLocaleDateString('en-ca')
-}
+
+    const infoContainer = document.createElement('div');
+    infoContainer.classList.add('contactUs');
+
+    const infoHeader = document.createElement('h3');
+    infoHeader.textContent = 'Our Info';
+
+    const phoneContainer = document.createElement('div');
+    const emailContainer = document.createElement('div');
+    const addressContainer = document.createElement('div');
+   
+    const phoneHeader = document.createElement('h4');
+    const emailHeader = document.createElement('h4');
+    const addressHeader = document.createElement('h4');
+
+    const phone = document.createElement('p');
+    const email = document.createElement('p');
+    const address = document.createElement('p');
+
+   
+    phoneHeader.textContent = 'Phone Number:';
+    emailHeader.textContent = 'Email:';
+    addressHeader.textContent = 'Address:';
+
+    phone.textContent = '020 7123 4567';
+    email.textContent = 'contact@therosebud.com';
+    address.textContent = 'Galactic Sector Alpha-7, Nebula Prime, Starhaven';
+
+    phoneContainer.append(phoneHeader, phone);
+    emailContainer.append(emailHeader, email);
+    addressContainer.append(addressHeader, address);
+
+    infoContainer.append(infoHeader, phoneContainer, emailContainer, addressContainer);
+
+    const reservationsContainer = document.createElement('div');
+    reservationsContainer.classList.add('contactUs');
+
+    const reservationHeader = document.createElement('h3');
+    reservationHeader.textContent = 'Enquire for reservation';
+    
+    const nameInput = document.createElement('input');
+    nameInput.type = 'text';
+    nameInput.placeholder = 'Name';
+
+    const emailInput = document.createElement('input');
+    emailInput.type = 'email';
+    emailInput.placeholder = 'Email';
+
+    const enquiry = document.createElement('textarea');
+    enquiry.rows = '8';
+    enquiry.resize = 'none';
+    enquiry.placeholder = 'Enquiry';
+
+    reservationsContainer.append(reservationHeader, nameInput, emailInput, enquiry);
+
+    rightCol.append(infoContainer, reservationsContainer);
+    
+};
